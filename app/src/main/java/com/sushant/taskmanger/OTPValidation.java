5a1a7e6db8acb75b22c73d10a19293251af469f8
sushantsuman <sushantcvrcebtech17@gmail.com>
2024-04-14 18:41:46 +0530

    addded otp activity

---
+++ app/src/main/java/com/sushant/taskmanger/OTPValidation.java
@@ -0,0 +1,14 @@
+package com.sushant.taskmanger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otpvalidation);
+    }
+}
